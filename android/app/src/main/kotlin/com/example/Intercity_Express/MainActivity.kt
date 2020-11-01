888abbec46795f560e62a8067244b881472671f4
vidurasilva <kvidurachathuranga@gmail.com>
2020-11-01 23:32:53 +0530

    Create project

---
+++ android/app/src/main/kotlin/com/example/Intercity_Express/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.Intercity_Express
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:Intercity_Express/route/route.dart';
+import 'package:Intercity_Express/store/store.dart';
+import 'package:Intercity_Express/theme/theme.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => Store(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Provider Example',
+        theme: appTheme,
+        initialRoute: '/splash',
+        routes: routes,
+      ),
+    );
+  }
+}
