e32c173949e354312afdec416b90eafeda98b674
vidurasilva <kvidurachathuranga@gmail.com>
2020-11-03 16:05:15 +0530

    splash

---
+++ lib/screens/splash_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+class SplashPage extends StatefulWidget {
+  @override
+  _SplashPageState createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 2), () {
+      onPressed:
+      Navigator.pushNamed(context, '/index');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: Container(
+          margin: EdgeInsets.only(top: 90),
+          child: Image.asset('assets/images/splash.png'),
+        ));
+  }
+}
