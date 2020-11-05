d030f94044e24a2be56edd7811596befaf9d1165
vidurasilva <kvidurachathuranga@gmail.com>
2020-11-05 23:24:07 +0530

    colors

---
+++ lib/style/colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+const Color bgColors = Colors.white;
+const Color textAreaPurpal = Color(0xff570454);
+
+//Button colors
+const Color buttonPurpal = Color(0xffda70d6);
+const Color buttonBlack = Color(0xffda70d6);
+
+//Text colors
+const Color textPurpal = Color(0xff570454);
+const Color textAreaGray = Colors.grey;
+const Color textYellow = Colors.yellow;
+const Color textWhite = Colors.white;
+const Color textBlack = Colors.black;
