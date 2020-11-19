40db6b3057abd50a7f809088efd85afcf2abeca8
vidurasilva <kvidurachathuranga@gmail.com>
2020-11-20 00:03:21 +0530

    ROUTE

--- lib/route/route.dart
+++ lib/route/route.dart
@@ -4,7 +4,6 @@ import '../screens/cart.dart';
 import '../screens/product_list.dart';
 import '../screens/single_product.dart';
 import '../screens/user.dart';
-
 import '../screens/splash_page.dart';
 
 Map<String, Widget Function(BuildContext)> routes = {
